c9e5f8feed64e769b66a5524c32c5f08e0179ed1
koo995 <107671886+koo995@users.noreply.github.com>
2024-09-27 14:38:32 +0900

    initial commit

---
+++ src/main/java/test/stock_my_example/StockMyExampleApplication.java
@@ -0,0 +1,13 @@
+package test.stock_my_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMyExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/stock_my_example/StockMyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package test.stock_my_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMyExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
