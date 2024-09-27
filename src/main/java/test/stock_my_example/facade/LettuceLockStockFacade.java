70102eeb3207eebf1a4d8ea0b522365ee4f23f7c
koo995 <107671886+koo995@users.noreply.github.com>
2024-09-27 22:58:15 +0900

    Redis 라이브러리의 장단점과 MySQL 비교
    실무에서 Redis?
    재시도가 필요하지 않은 lock 은 lettuce 활용
    재시도가 필요한 경우에는 redisson 를 활용
    혼용해서 사용한다.
    
    MySQL
    이미 Mysql 을 사용하고 있다면 별도의 비용없이 사용가능하다.
    어느정도의 트래픽까지는 문제없이 활용이 가능하다.
    Redis 보다는 성능이 좋지않다.
    
    Redis
    활용중인 Redis 가 없다면 별도의 구축비용과 인프라 관리비용이 발생한다.
    Mysql 보다 성능이 좋다. 따라서 더 많은 요청을 처리할 수 있다.
    비용적 여유가 있고 Mysql로 처리불가능한 트래픽이라는 redis을 사용한다.

--- src/main/java/test/stock_my_example/facade/LettuceLockStockFacade.java
+++ src/main/java/test/stock_my_example/facade/LettuceLockStockFacade.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Component;
 import test.stock_my_example.repository.RedisLockRepository;
 import test.stock_my_example.service.StockService;
 
+/**
+ * 구현이 간단하다
+ * spring data redis 를 이용하면 lettuce 가 기본이기때문에 별도의 라이브러리를 사용하지 않아도 된다.
+ * spin lock 방식이기때문에 동시에 많은 스레드가 lock 획득 대기 상태라면 redis 에 부하가 갈 수 있다.
+ */
 @Component
 public class LettuceLockStockFacade {
     private final RedisLockRepository redisLockRepository;
--- src/main/java/test/stock_my_example/facade/RedissonLockStockFacade.java
+++ src/main/java/test/stock_my_example/facade/RedissonLockStockFacade.java
@@ -7,6 +7,12 @@ import test.stock_my_example.service.StockService;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 락 획득 재시도를 기본으로 제공한다.
+ * pub-sub 방식으로 구현이 되어있기 때문에 lettuce 와 비교했을 때 redis 에 부하가 덜 간다.
+ * 별도의 라이브러리를 사용해야한다.
+ * lock 을 라이브러리 차원에서 제공해주기 떄문에 사용법을 공부해야 한다.
+ */
 @Component
 public class RedissonLockStockFacade {
     private final RedissonClient redissonClient;
